bd38612b1d202123cd191c9e10eaf2a9c139dc39
Hoesung <hoesung@Hoesungs-Mac-mini.local>
2023-01-30 15:45:09 +0900

    0130

---
+++ 09_utilization_of_class/01_overloading.cpp
@@ -0,0 +1,25 @@
+
+/*
+연산자 오버로딩 
+사용자 정의에 확대시키고 싶다. 
+Ex) 
+* : 포인터 값 or 곱하기 ... s
+*/
+
+#include<iostream>
+#include"time.h"
+using namespace std;
+
+int main() {
+	Time day1(1, 40);
+	Time day2(2, 30);
+	
+	day1.show();
+	day2.show();
+
+	Time total;
+	total = day1.operator+(day2);
+	total.show();
+
+	return 0;
+}
